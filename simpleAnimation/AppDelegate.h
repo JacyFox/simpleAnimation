69d67522966cd5e4741e1902106afa6b697cade3
郇建忠 <huanjianzhong@lakala.com>
2015-09-14 09:25:25 +0800

    Initial Commit

---
+++ simpleAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  simpleAnimation
+//
+//  Created by lakala on 15/9/14.
+//  Copyright (c) 2015年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ simpleAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  simpleAnimation
+//
+//  Created by lakala on 15/9/14.
+//  Copyright (c) 2015年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
